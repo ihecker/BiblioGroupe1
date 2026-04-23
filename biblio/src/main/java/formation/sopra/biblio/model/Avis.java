279cd8968887b4940d045450fb4d120209c74483
AxelleDrd <axelle.drouard@orange.fr>
2026-04-23 10:36:10 +0200

    Création classe Avis

---
+++ biblio/src/main/java/formation/sopra/biblio/model/Avis.java
@@ -0,0 +1,90 @@
+package formation.sopra.biblio.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="avis")
+public class Avis {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="avis_id")
+    private Integer id;
+
+    private Integer note;
+
+    private String commentaire;
+    
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name="livre_id")
+    private Livre livre;
+    
+    public Avis(Integer id, int note, String commentaire, LocalDate date, Livre livre) {
+        this.id = id;
+        this.note = note;
+        this.commentaire = commentaire;
+        this.date = date;
+        this.livre = livre;
+    }
+
+    public Avis(int note, String commentaire, LocalDate date, Livre livre) {
+        this.note = note;
+        this.commentaire = commentaire;
+        this.date = date;
+        this.livre = livre;
+    }
+
+    public Avis() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public void setNote(int note) {
+        this.note = note;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Livre getLivre() {
+        return livre;
+    }
+
+    public void setLivre(Livre livre) {
+        this.livre = livre;
+    }
+
+}
