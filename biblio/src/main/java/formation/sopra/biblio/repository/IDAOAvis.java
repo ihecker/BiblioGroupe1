153a2fa26930384c26bfc68fbbde4886086ffb6d
AxelleDrd <axelle.drouard@orange.fr>
2026-04-23 11:09:53 +0200

    Creation IDAOAvis + toString

---
+++ biblio/src/main/java/formation/sopra/biblio/repository/IDAOAvis.java
@@ -0,0 +1,8 @@
+package formation.sopra.biblio.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import formation.sopra.biblio.model.Avis;
+
+public interface IDAOAvis extends JpaRepository<Avis, Integer> {
+
+}
