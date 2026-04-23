830aab1111530a9502b584a1d89c275e1c8f441a
AxelleDrd <axelle.drouard@orange.fr>
2026-04-23 17:06:03 +0200

    Auto stash before checking out "origin/features/Axelle/Avis-angular"

--- biblio-angular/src/app/page/genre/genre-page/genre-page.ts
+++ biblio-angular/src/app/page/genre/genre-page/genre-page.ts
@@ -17,6 +17,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './genre-page.html',
   styleUrl: './genre-page.css',
 })
+<<<<<<< Updated upstream
 export class GenrePage implements OnInit {
   private genreService: GenreService = inject(GenreService);
   private formBuilder: FormBuilder = inject(FormBuilder);
