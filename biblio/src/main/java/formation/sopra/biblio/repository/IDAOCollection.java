c6c3e8da2ae95dead6df55194adbb1bd943453c6
Marie <malestro56@gmail.com>
2026-04-23 11:54:50 +0200

    Ajout CollectionNotFoundException + IDAOCollection

---
+++ biblio/src/main/java/formation/sopra/biblio/repository/IDAOCollection.java
@@ -0,0 +1,9 @@
+package formation.sopra.biblio.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IDAOCollection extends JpaRepository<Collection, Integer>{
+    
+}
