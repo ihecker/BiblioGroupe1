290a50237843757f475d1af23bea3eb2f91f6e38
ihecker <irwin.hecker@outlook.com>
2026-04-23 12:08:27 +0200

    AJOUT - DTO LIVRE

--- biblio/src/main/java/formation/sopra/biblio/dto/livre/request/LivreRequest.java
+++ biblio/src/main/java/formation/sopra/biblio/dto/livre/request/LivreRequest.java
@@ -1,5 +1,99 @@
 package formation.sopra.biblio.dto.livre.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class LivreRequest {
+    @NotNull
+    private String titre;
+
+    @NotNull
+    private String resume;
+
+    @NotNull
+    private int annee;
+
+    @Min(value = 1, message = "Un ID ne peut être ni nul ni négatif")
+    private Integer idAuteur;
+
+    @Min(value = 1, message = "Un ID ne peut être ni nul ni négatif")
+    private Integer idEditeur;
+
+    @Min(value = 1, message = "Un ID ne peut être ni nul ni négatif")
+    private Integer idCollection;
+
+    @Min(value = 1, message = "Un ID ne peut être ni nul ni négatif")
+    private Integer idGenre;
+
+    public LivreRequest() {
+    }
+
+    public LivreRequest(String titre, String resume, int annee, Integer idAuteur, Integer idEditeur,
+            Integer idCollection,
+            Integer idGenre) {
+        this.titre = titre;
+        this.resume = resume;
+        this.annee = annee;
+        this.idAuteur = idAuteur;
+        this.idEditeur = idEditeur;
+        this.idCollection = idCollection;
+        this.idGenre = idGenre;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public Integer getIdAuteur() {
+        return idAuteur;
+    }
+
+    public void setIdAuteur(Integer idAuteur) {
+        this.idAuteur = idAuteur;
+    }
+
+    public Integer getIdEditeur() {
+        return idEditeur;
+    }
+
+    public void setIdEditeur(Integer idEditeur) {
+        this.idEditeur = idEditeur;
+    }
+
+    public Integer getIdCollection() {
+        return idCollection;
+    }
+
+    public void setIdCollection(Integer idCollection) {
+        this.idCollection = idCollection;
+    }
+
+    public Integer getIdGenre() {
+        return idGenre;
+    }
+
+    public void setIdGenre(Integer idGenre) {
+        this.idGenre = idGenre;
+    }
 
 }
--- biblio/src/main/java/formation/sopra/biblio/dto/livre/response/LivreResponse.java
+++ biblio/src/main/java/formation/sopra/biblio/dto/livre/response/LivreResponse.java
@@ -1,5 +1,111 @@
 package formation.sopra.biblio.dto.livre.response;
 
+import formation.sopra.biblio.model.Livre;
+
 public class LivreResponse {
+
+    private Integer id;
+    private String titre;
+    private String resume;
+    private int annee;
+
+    private Integer idAuteur;
+
+    private Integer idEditeur;
+
+    private Integer idCollection;
+
+    private Integer idGenre;
+
+    public LivreResponse() {
+    }
+
+    public LivreResponse(Integer id, String titre, String resume, int annee, Integer idAuteur,
+            Integer idEditeur, Integer idCollection, Integer idGenre) {
+        this.id = id;
+        this.titre = titre;
+        this.resume = resume;
+        this.annee = annee;
+        this.idAuteur = idAuteur;
+        this.idEditeur = idEditeur;
+        this.idCollection = idCollection;
+        this.idGenre = idGenre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public Integer getIdAuteur() {
+        return idAuteur;
+    }
+
+    public void setIdAuteur(Integer idAuteur) {
+        this.idAuteur = idAuteur;
+    }
+
+    public Integer getIdEditeur() {
+        return idEditeur;
+    }
+
+    public void setIdEditeur(Integer idEditeur) {
+        this.idEditeur = idEditeur;
+    }
+
+    public Integer getIdCollection() {
+        return idCollection;
+    }
+
+    public void setIdCollection(Integer idCollection) {
+        this.idCollection = idCollection;
+    }
+
+    public String getIdGenre() {
+        return idGenre;
+    }
+
+    public void setIdGenre(String idGenre) {
+        this.idGenre = idGenre;
+    }
+
+    public static LivreResponse convert(Livre l) {
+        LivreResponse response = new LivreResponse();
+        response.setId(l.getId());
+        response.setResume(l.getResume());
+        response.setAnnee(l.getAnnee());
+        response.setIdAuteur(l.getAuteur().getId());
+        response.setIdEditeur(l.getEditeur().getId());
+        response.setIdCollection(l.getCollection().getId());
+        response.setIdGenre(l.getGenre().getId());
+
+    }
 
 }
