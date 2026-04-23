f1cc242eb179da4f110eea3cde04a8c8095f4d77
AxelleDrd <axelle.drouard@orange.fr>
2026-04-23 17:44:21 +0200

    Liste déroulante choix livre (en cours)

--- biblio-angular/src/app/page/avis/avis-page/avis-page.ts
+++ biblio-angular/src/app/page/avis/avis-page/avis-page.ts
@@ -2,37 +2,44 @@ import { Component, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AvisService } from '../../../service/avis/avis-service';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
-import { Observable, Subject } from 'rxjs';
+import { Observable, startWith, Subject, switchMap } from 'rxjs';
 import { Avis } from '../../../model/avis';
 import { CommonModule } from '@angular/common';
+import { Livre } from '../../../model/livre';
+import { LivreService } from '../../../service/livre-service';
 
 @Component({
   selector: 'app-avis-page',
-  imports: [FormsModule,CommonModule, ReactiveFormsModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './avis-page.html',
   styleUrl: './avis-page.css',
 })
 export class AvisPage {
   private titleService: Title = inject(Title);
   private avisService: AvisService = inject(AvisService);
+  private livreService: LivreService = inject(LivreService);
   private formBuilder: FormBuilder = inject(FormBuilder);
 
   protected avis$!: Observable<Avis[]>;
   protected refresh$: Subject<void> = new Subject<void>();
-  //protected livres$!: Observable<Livre[]>;
+  protected livres$!: Observable<Livre[]>;
   protected update: boolean = false;
 
   protected formAvis!: FormGroup;
   protected formNoteCtrl!: FormControl;
   protected formCommentaireCtrl!: FormControl;
-  protected formDateCtrl: FormControl = new FormControl((new Date()).toISOString().substring(0,10));
-  protected formLivreIdCtrl!: FormControl;
-  protected formLivreTitreCtrl!: FormControl;
+  protected formDateCtrl: FormControl = new FormControl((new Date()).toISOString().substring(0, 10));
+  protected formLivreCtrl!: FormControl;
 
   protected updatedAvis: Avis = { id: 0, note: 0, commentaire: "", date: "", livreId: 0, livreTitre: "" };
 
   ngOnInit(): void {
     this.titleService.setTitle("Avis");
+
+    this.avis$ = this.refresh$.pipe(
+      startWith(0), switchMap(() => this.avisService.findAll())
+    );
+    this.livres$ = this.livreService.findAll();
 
     const noteValueValidator = (control: AbstractControl): ValidationErrors | null => {
       if (control.value < 0 || control.value > 10) {
@@ -44,15 +51,13 @@ export class AvisPage {
     this.formNoteCtrl = this.formBuilder.control("", [Validators.required, noteValueValidator]);
     //this.formDateCtrl = this.formBuilder.control("", Validators.required);
     this.formCommentaireCtrl = this.formBuilder.control("", Validators.required);
-    this.formLivreIdCtrl = this.formBuilder.control("", Validators.required);
-    this.formLivreTitreCtrl = this.formBuilder.control("", Validators.required);
-
+    this.formLivreCtrl = this.formBuilder.control("", Validators.required);
+    
     this.formAvis = this.formBuilder.group({
       note: this.formNoteCtrl,
       commentaire: this.formCommentaireCtrl,
       date: this.formDateCtrl,
-      livreId: this.formLivreIdCtrl,
-      livreTitre: this.formLivreTitreCtrl
+      livre: this.formLivreCtrl.value
     });
 
   }
@@ -77,8 +82,8 @@ export class AvisPage {
       note: this.formNoteCtrl.value,
       commentaire: this.formCommentaireCtrl.value,
       date: this.formDateCtrl.value,
-      livreId: this.formLivreIdCtrl.value,
-      livreTitre: this.formLivreTitreCtrl.value
+      livreId: this.formLivreCtrl.value.id,
+      livreTitre: this.formLivreCtrl.value.titre
     };
 
     this.avisService.add(avis).subscribe(() => this.reload());
