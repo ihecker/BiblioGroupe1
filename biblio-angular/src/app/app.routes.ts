9e1dc62291c43030cccd7eac966fab96b296566e
ihecker <irwin.hecker@outlook.com>
2026-04-23 16:50:44 +0200

    WIP - PAGE LIVRE

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { AuteurPage } from './page/auteur/auteur-page/auteur-page';
 import { GenrePage } from './page/genre/genre-page/genre-page';
 import { EditeurPage } from './page/editeur/editeur-page/editeur-page';
+import { LivrePage } from './page/livre/livre-page/livre-page';
 
 export const routes: Routes = [
+  {path:"livres", component: LivrePage},
   {path:"auteur", component: AuteurPage},
   {path:"genre", component: GenrePage},
   {path:"editeur", component: EditeurPage},
