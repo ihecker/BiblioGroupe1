046321ab2583a1d63491b035bf04faee1ace2c40
Marie <malestro56@gmail.com>
2026-04-24 15:28:20 +0200

    ajout bouton modifier pour les collections

--- biblio-angular/src/app/page/collection/collection-page/collection-page.ts
+++ biblio-angular/src/app/page/collection/collection-page/collection-page.ts
@@ -1,14 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { CollectionService } from '../../../service/collection/collection-service';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Observable, startWith, Subject, switchMap } from 'rxjs';
 import { Collection } from '../../../model/collection';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-collection-page',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './collection-page.html',
   styleUrl: './collection-page.css',
 })
@@ -21,6 +21,7 @@ private formBuilder: FormBuilder = inject(FormBuilder);
 
 protected collection$!: Observable<Collection[]>;
 private refresh$: Subject<void> = new Subject<void>();
+protected collectionEnEdition: Collection = {} as Collection;
 
 protected formCollection!: FormGroup;
 protected formNomCtrl!: FormControl;
@@ -55,8 +56,31 @@ public addCollection() {
   this.collectionService.add(collection).subscribe(()=> this.reload());
 }
 
+public updateCollection(collection: Collection){
+this.collectionEnEdition =  {} as Collection;
+this.collectionEnEdition.id= collection.id;
+this.collectionEnEdition.nom = collection.nom; 
+}
+
+  annuler() {
+  this.collectionEnEdition.id = -1;
+}
+
 public deleteCollection(collection: Collection) {
   this.collectionService.deleteById(collection.id).subscribe(()=> this.reload());
 }
+
+ editer(collection: Collection) {
+    this.collectionEnEdition = { ...collection};
+  }
+  
+sauvegarder() {
+  this.collectionService.update(this.collectionEnEdition!.id, this.collectionEnEdition!).subscribe(() => {
+    this.collectionEnEdition.id = -1;
+    this.reload();
+  });
+}
+
+
 }
 
