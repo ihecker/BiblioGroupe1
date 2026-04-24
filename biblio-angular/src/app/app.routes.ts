30f942ab2d0f0434d4444e7cc8b4ae779f036772
Marie <malestro56@gmail.com>
2026-04-24 09:37:24 +0200

    Ajout collection à routes.ts

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuteurPage } from './page/auteur/auteur-page/auteur-page';
 import { GenrePage } from './page/genre/genre-page/genre-page';
 import { EditeurPage } from './page/editeur/editeur-page/editeur-page';
+import { CollectionPage } from './page/collection/collection-page/collection-page';
 
 export const routes: Routes = [
   {path:"auteur", component: AuteurPage},
   {path:"genre", component: GenrePage},
   {path:"editeur", component: EditeurPage},
+  {path:"collection", component: CollectionPage},
   {path:"", component:AuteurPage, pathMatch:"full"}
 ];
